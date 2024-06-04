dc125b72acc4370fad6aaa3a39cf922e35400927
Roi Shukrun <roishukrun8@gmail.com>
2024-06-04 13:03:56 +0300

    filters and thumbnail update

--- asp-tech-titans/src/main-page/mainPage.js
+++ asp-tech-titans/src/main-page/mainPage.js
@@ -5,31 +5,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Filters from './components/filters/filters';
 import Sidebar from './components/side-bar/sideBar';
 import Header from './components/header/header';
-import Recommended from './components/video-thumbnail/recommended-videos/recommended';
-import VideoCard from '../video-watch-page/components/related-videos/videoCard.js'
 
 const MainPage = ({ setUrl }) => {
   return (
     <div className="container-fluid">
-      <div className="row">
+      <div className="row no-gutters">
         <div className="col-md-2 p-0">
           <Sidebar />
         </div>
         <div className="col-md-10 p-0">
           <Header />
-          <div className="container-fluid">
+          <div className="container-fluid p-0">
             <Filters />
-            <Recommended />
-            <div className="row">
+            <div className="row no-gutters">
               {jsonData.map((video, index) => (
-                <div key={index} className="col-md-3 mb-4">
+                <div key={index} className="col-md-3 p-1"> {/* Adjust padding to reduce space */}
                   <VideoThumbnail video={video} onClick={() => setUrl(video.videoUrl)} />
                 </div>
               ))}
             </div>
-            <div className="row">
+            <div className="row no-gutters">
               {jsonData.map((video, index) => (
-                <div key={index} className="col-md-3 mb-4">
+                <div key={index} className="col-md-3 p-1"> {/* Adjust padding to reduce space */}
                   <VideoThumbnail video={video} onClick={() => setUrl(video.videoUrl)} />
                 </div>
               ))}
