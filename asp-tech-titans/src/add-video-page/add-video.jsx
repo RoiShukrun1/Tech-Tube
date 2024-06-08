6b9c7be0cf14704e6ccb3b01846cbd63c4a963bc
kobi <kobi123545@gmail.com>
2024-06-08 23:04:02 +0300

    added upload page and make some cosmetic changes to add video page

--- asp-tech-titans/src/add-video-page/add-video.jsx
+++ asp-tech-titans/src/add-video-page/add-video.jsx
@@ -1,62 +1,76 @@
 import React, { useState } from 'react';
 import './add-video.css';
 import { ReactComponent as AddVideoIcon } from '../images/addVideo.svg';
-import { ReactComponent as ImageIcon } from '../images/addimage.svg'; // Replace '../images/image.svg' with the path to your image icon
-export const AddVideo = () => {
+import { ReactComponent as ImageIcon } from '../images/addimage.svg';
 
+export const AddVideo = () => {
     const [image, setImage] = useState(null);
+    const [video, setVideo] = useState(null);
+
     const handleImageUpload = (event) => {
         setImage(URL.createObjectURL(event.target.files[0]));
-      }
-    const [video, setVideo] = useState(null);
+    };
+
     const handleVideoUpload = (event) => {
         setVideo(URL.createObjectURL(event.target.files[0]));
-        }
+    };
 
     return (
         <div className="container">
-            <form>
-            <h1>Details:</h1>
-            <textarea 
-            className = "title-container"
-            placeholder="Enter your title"
-            id="title"
-            name="videoTitle">
-            </textarea>
+            <div>
+                <h1>Details:</h1>
+                <textarea 
+                    className="title-container"
+                    placeholder="Enter your title"
+                    id="title"
+                    name="videoTitle"
+                ></textarea>
 
-            <textarea 
-            className = "description-container"
-            placeholder="Enter your description"
-            id="descripton" 
-            name="videoDescription">
-            </textarea>
+                <textarea 
+                    className="description-container"
+                    placeholder="Enter your description"
+                    id="description"
+                    name="videoDescription"
+                ></textarea>
 
-            <textarea 
-            className = "tags-container"
-            placeholder="Enter tags"
-            id="tags"
-            name="tags">
-            </textarea>
+                <textarea 
+                    className="tags-container"
+                    placeholder="Enter tags"
+                    id="tags"
+                    name="tags"
+                ></textarea>
 
-            <label htmlFor="videoUpload">
-            <div className="addVideo">
-            {!video && <AddVideoIcon className="addicon" />}
-            <input type="file" onChange={handleVideoUpload} id="videoUpload" name="videoUpload" accept="video/mp4" style={{display: 'none'}} />
-            <div>{video && <video src={video} alt="User uploaded "style={{width: '900px', height: '500px'}} />}</div>
-            </div>
-            </label>
-
-            <label htmlFor="image" className="thumbnail-label">
-                <div className="thumbnail">
-                    <p>Thumbnail:</p>
-                    {!image && <ImageIcon className="ImageIcon" />}
-                    <input type="file" onChange={handleImageUpload} id="image" name="image" accept="image/*" style={{display: 'none'}} />
-                    <div>{image && <img src={image} alt="User uploaded "style={{width: '150px', height: '100px'}} />}</div>
+                <div className="category-container">
+                    <h2>Playlist:</h2>
+                    <select name="category" id="category">
+                        <option value="category">Select playlist</option>
+                        <option value="category">Music</option>
+                        <option value="category">Sport</option>
+                        <option value="category">Study</option>
+                    </select>
                 </div>
-            </label>
-            </form>
+                <label htmlFor="image" className="thumbnail-label">
+                    <div className="thumbnail">
+                    <h3>thumbnail:</h3>
+                        {!image && <ImageIcon className="ImageIcon" />}
+                        <input type="file" onChange={handleImageUpload} id="image" name="image" accept="image/*" style={{ display: 'none' }} />
+                        <div>{image && <img src={image} alt="User uploaded" className="image-preview" />}</div>
+                    </div>
+                </label>
+            </div>
+            
+            <div className="media-container">
+                <label htmlFor="videoUpload">
+                    <div className="addVideo">
+                        {!video && <AddVideoIcon className="addicon" />}
+                        <input type="file" onChange={handleVideoUpload} id="videoUpload" name="videoUpload" accept="video/mp4" style={{ display: 'none' }} />
+                        <div>{video && <video src={video} controls className="video-preview" />}</div>
+                    </div>
+                </label>
+            </div>
+            <button className="Upload-button">Upload</button>
         </div>
-
     );
 }
+
 export default AddVideo;
---
+++ asp-tech-titans/src/add-video-page/upload-video.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useRef } from 'react';
+import './upload-video.css';
+import { ReactComponent as UploadIcon } from '../images/addVideo.svg'; // Make sure to replace this path with the correct path to your SVG
+
+const UploadPage = () => {
+  const [video, setVideo] = useState(null);
+  const videoInputRef = useRef(null);
+
+  const handleVideoUpload = (event) => {
+    setVideo(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const handleButtonClick = () => {
+    videoInputRef.current.click();
+  };
+  
+  return (
+    <div className="container">
+      <div className={`upload-container ${video ? 'hidden' : ''}`}>
+        <div className="upload-box">
+          <label htmlFor="videoUpload" className="upload-label">
+            <div className="upload-icon-container">
+              {!video && <UploadIcon className="upload-icon" />}
+            </div>
+            <div className="upload-instructions">
+              Drag and drop video files to upload
+            </div>
+            <input
+              type="file"
+              onChange={handleVideoUpload}
+              ref={videoInputRef}
+              id="videoUpload"
+              accept="video/mp4"
+              style={{ display: 'none' }}
+            />
+          </label>
+        </div>
+      </div>
+      <button className="upload-button" type="button" onClick={handleButtonClick}>SELECT FILE</button>
+      {video && <video src={video} className="video-preview" controls />}
+      <button className="next-button" type="button">Next</button>
+    </div>
+  );
+};
+
+export default UploadPage;
