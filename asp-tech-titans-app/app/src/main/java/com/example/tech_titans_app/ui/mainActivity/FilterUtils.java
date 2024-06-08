84e543288fefb2e785e0d71a93782f2e03d93183
Roi Shukrun <roishukrun8@gmail.com>
2024-06-08 17:52:06 +0300

    app main page update

---
+++ asp-tech-titans-app/app/src/main/java/com/example/tech_titans_app/ui/mainActivity/FilterUtils.java
@@ -0,0 +1,42 @@
+package com.example.tech_titans_app.ui.mainActivity;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.tech_titans_app.R;
+
+public class FilterUtils {
+
+    private TextView lastSelectedFilter;
+
+    public void setupFilterClickListeners(View rootView) {
+        View.OnClickListener filterClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (lastSelectedFilter != null) {
+                    lastSelectedFilter.setSelected(false);
+                }
+                view.setSelected(true);
+                lastSelectedFilter = (TextView) view;
+            }
+        };
+
+        rootView.findViewById(R.id.all_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.computer_programming_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.music_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.gaming_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.restaurants_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.mixes_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.barbells_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.sitcoms_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.satire_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.brawl_stars_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.functions_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.playlists_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.uefa_champions_league_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.cdj_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.news_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.live_filter).setOnClickListener(filterClickListener);
+        rootView.findViewById(R.id.sports_filter).setOnClickListener(filterClickListener);
+    }
+}
