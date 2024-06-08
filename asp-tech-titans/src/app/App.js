3b5989a8c5632f04c34ea80f2801df78e612a50f
Roi Shukrun <roishukrun8@gmail.com>
2024-06-08 13:11:04 +0300

    app main page update

--- asp-tech-titans/src/app/App.js
+++ asp-tech-titans/src/app/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import VideoWatchPage from '../video-watch-page/video-watch-page';
+import MainPage from '../main-page/mainPage'
 
 function App() {
 
   return (
     <div>
-      <VideoWatchPage initVideoUrl={'/db/videos/1Digitalization; Where to start_.mp4'} />
+      {/* <VideoWatchPage initVideoUrl={'/db/videos/1Digitalization; Where to start_.mp4'} /> */}
+       <MainPage/>
     </div>
   );
 }
