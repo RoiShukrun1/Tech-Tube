47230b1fc71529e38527f9070f053b5608d9b316
Aviel Segev <aviel281999@gmail.com>
2024-06-10 16:41:23 +0300

    Added video related in the bottom and also add icons to buttons

--- asp-tech-titans-app/app/src/main/java/com/example/tech_titans_app/ui/VideoPlayer.java
+++ asp-tech-titans-app/app/src/main/java/com/example/tech_titans_app/ui/VideoPlayer.java
@@ -6,11 +6,19 @@ import android.widget.MediaController;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.tech_titans_app.R;
+import com.example.tech_titans_app.ui.adapters.VideosListAdapter;
 import com.example.tech_titans_app.ui.entities.Video;
+import com.example.tech_titans_app.ui.viewmodels.MainVideoViewModel;
 
 public class VideoPlayer extends AppCompatActivity {
     private Video currentVideo;
+    private VideosListAdapter adapter;
+    private MainVideoViewModel videoViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,15 @@ public class VideoPlayer extends AppCompatActivity {
 
         // Start the video
         videoView.start();
+
+        RecyclerView lstVideos = findViewById(R.id.listVideosVWP);
+        lstVideos.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new VideosListAdapter();
+        lstVideos.setAdapter(adapter);
+
+        videoViewModel = new ViewModelProvider(this).get(MainVideoViewModel.class);
+        videoViewModel.getAllVideos().observe(this, videos -> adapter.setVideos(videos));
     }
 
 }
