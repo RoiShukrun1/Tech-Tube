13f3977178a8bcb1fec12779dab4caaaa2650161
kobi <kobi123545@gmail.com>
2024-06-16 21:18:24 +0300

    login validation added and added - add video implemtation

---
+++ asp-tech-titans-app/app/src/main/java/com/example/tech_titans_app/ui/utilities/LoggedIn.java
@@ -0,0 +1,29 @@
+package com.example.tech_titans_app.ui.utilities;
+
+import com.example.tech_titans_app.ui.models.account.AccountData;
+
+public class LoggedIn {
+    private static LoggedIn instance;
+    private AccountData loggedInUser;
+
+    private LoggedIn() {}
+
+    public static synchronized LoggedIn getInstance() {
+        if (instance == null) {
+            instance = new LoggedIn();
+        }
+        return instance;
+    }
+
+    public AccountData getLoggedInUser() {
+        return loggedInUser;
+    }
+    public void setLoggedInUser(AccountData loggedInUser) {
+        this.loggedInUser = loggedInUser;
+    }
+
+    public void loggout() {
+        this.loggedInUser = null;
+    }
+
+}
