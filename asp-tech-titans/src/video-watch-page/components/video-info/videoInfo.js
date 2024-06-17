c8379799f8816d4b7b8f3f126487b9782a68e257
Aviel Segev <aviel281999@gmail.com>
2024-06-18 00:27:04 +0300

    update databse logic names and add name change option to video by owner user

--- asp-tech-titans/src/video-watch-page/components/video-info/videoInfo.js
+++ asp-tech-titans/src/video-watch-page/components/video-info/videoInfo.js
@@ -3,6 +3,12 @@ import './videoInfo.css';
 import PublisherInfo from './publisherInfo';
 import { ReactComponent as Download } from './download.svg';
 import { ReactComponent as Share } from './share.svg';
+import { useContext } from 'react';
+import { ThemeContext } from '../../../contexts/themeContext';
+import { ReactComponent as Pencil } from './pencil.svg';
+import { VideoDataContext } from '../../../contexts/videoDataContext';
+import { useState } from 'react';
+
 
 function copyUrl() {
     const url = window.location.href;
@@ -10,6 +16,47 @@ function copyUrl() {
 }
 
 function VideoInfo({ currentVideo, currentUser, setUsers, setMoreInfoPressed, moreInfoPressed }) {
+
+    const { darkMode } = useContext(ThemeContext);
+    const [isPencilClicked, setIsPencilClicked] = useState(false);
+    const [VideoTitleInputValue, setVideoTitleInputValue] = useState(currentVideo.title);
+
+    const {videoData, setVideoData} = useContext(VideoDataContext);
+
+    const handleVideoTitleInputChange = (event) => {
+        setVideoTitleInputValue(event.target.value);
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            closeInput();
+        }
+    };
+
+    const setNewVideoTitle = (newTitle) => {
+        setVideoData(prevVideos => {
+            const updatedVideos = [...prevVideos];
+    
+            const thisVideo = updatedVideos.find(video => video.id === currentVideo.id);
+    
+            if (thisVideo) {
+                thisVideo.title = newTitle;
+            }
+    
+            return updatedVideos;
+        });
+    };
+
+    const closeInput = () => {
+        setNewVideoTitle(VideoTitleInputValue);
+        setIsPencilClicked(false);
+    };
+
+    const inputIsEmpty = () => { return VideoTitleInputValue === '' ? true : false; };
+
+    const currentUserIsOwnerOfVideo = () => {
+        return currentUser && currentUser.username === currentVideo.publisher;
+    }
 
     const download = () => {
         const downloadUrl = currentVideo.videoUploaded;
@@ -33,17 +80,28 @@ function VideoInfo({ currentVideo, currentUser, setUsers, setMoreInfoPressed, mo
 
     return (
         <div>
-            <h1 className="title">{title}</h1>
+            <h1 className="title">{isPencilClicked ? <input 
+            className='inputVideoTitle'
+            onChange={handleVideoTitleInputChange}
+            onKeyDown={handleKeyDown}
+            value={VideoTitleInputValue}
+            /> 
+            : title}
+                {currentUser && currentUserIsOwnerOfVideo()
+                && <button type="button" className='pencil-button' onClick={() => setIsPencilClicked(true)}>
+                    <Pencil />
+                </button>}
+            </h1>
             <PublisherInfo publisherImage={publisherImage} publisher={publisher}
                 setUsers={setUsers} currentUser={currentUser} />
             <button type="button" className="btn btn-light download-button"
                 onClick={download}><Download /> Download
             </button>
 
-            <div className="btn-group" role="group" style={{float: 'right'}}>
+            <div className="btn-group" role="group" style={{ float: 'right' }}>
                 <button type="button"
                     className="btn btn-light share-button dropdown-toggle"
-                    data-bs-toggle="dropdown" 
+                    data-bs-toggle="dropdown"
                     aria-expanded="false"><Share /> Share
                 </button>
                 <ul className="dropdown-menu">
