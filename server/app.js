0c95f85287983dfae915252f8521b3537610cd4c
Aviel Segev <aviel281999@gmail.com>
2024-06-30 23:19:37 +0300

    add route handler

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 // import mongoose from 'mongoose';
-// import routerComments from './router/comments.js';
+import routerComments from './routes/comments.js';
 
 const server = express();
-const port = 80;
+const port = 5001;
 
 server.use(bodyParser.json);
 
-// server.use('/api/Comments', routerComments);
+server.use('/api/Comments', routerComments);
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
